0e693afc27e83a97535572d3f68e7da200b12dc8
Farkas Levente <farkas.levente2004@gmail.com>
2024-03-07 13:16:44 +0100

    vU: .env and index updated with localhost options

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { cookie_keys } = require('./src/config/auth.config')
 const cookieSession = require('cookie-session')
 
 const PORT = 9090;
-const HOST = process.env.HOST302loc || "localhost";
+const HOST = process.env.HOST202loc || "localhost";
 
 
 const app = express()
